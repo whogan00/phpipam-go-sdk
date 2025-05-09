481f2ddc52b2d8e5919d7eeeec92a2aa8c161bea
William Hogan <william_hogan@bmc.com>
2025-05-09 18:22:34 -0400

    fix: id is int not string

--- pkg/phpipam/addresses.go
+++ pkg/phpipam/addresses.go
@@ -2,36 +2,36 @@ package phpipam
 
 import (
 	"fmt"
-	"strconv"
 	"net/url"
+	"strconv"
 )
 
 // Address represents a phpIPAM address object
 type Address struct {
-	ID          string `json:"id,omitempty"`
-	SubnetID    string `json:"subnetId,omitempty"`
+	ID          int    `json:"id,omitempty"`
+	SubnetID    int    `json:"subnetId,omitempty"`
 	IP          string `json:"ip,omitempty"`
-	IsGateway   string `json:"is_gateway,omitempty"`
+	IsGateway   int    `json:"is_gateway,omitempty"`
 	Description string `json:"description,omitempty"`
 	Hostname    string `json:"hostname,omitempty"`
 	Mac         string `json:"mac,omitempty"`
 	Owner       string `json:"owner,omitempty"`
-	Tag         string `json:"tag,omitempty"`
-	PTRIgnore   string `json:"PTRignore,omitempty"`
-	PTR         string `json:"PTR,omitempty"`
-	DeviceID    string `json:"deviceId,omitempty"`
+	Tag         int    `json:"tag,omitempty"`
+	PTRIgnore   int    `json:"PTRignore,omitempty"`
+	PTR         int    `json:"PTR,omitempty"`
+	DeviceID    int    `json:"deviceId,omitempty"`
 	Port        string `json:"port,omitempty"`
 	Note        string `json:"note,omitempty"`
 	LastSeen    string `json:"lastSeen,omitempty"`
-	ExcludePing string `json:"excludePing,omitempty"`
+	ExcludePing int    `json:"excludePing,omitempty"`
 	EditDate    string `json:"editDate,omitempty"`
 }
 
 // Tag represents an IP address tag
 type Tag struct {
-	ID          string `json:"id,omitempty"`
+	ID          int    `json:"id,omitempty"`
 	Type        string `json:"type,omitempty"`
-	ShowTag     string `json:"showtag,omitempty"`
+	ShowTag     int    `json:"showtag,omitempty"`
 	BgColor     string `json:"bgcolor,omitempty"`
 	FgColor     string `json:"fgcolor,omitempty"`
 	DisplayName string `json:"displayname,omitempty"`
@@ -49,10 +49,19 @@ func NewAddressesService(client *Client) *AddressesService {
 }
 
 // Get returns a specific address by ID
-func (a *AddressesService) Get(id string) (*Address, error) {
+func (a *AddressesService) Get(id int) (*Address, error) {
 	var address Address
-	_, err := a.client.Request("GET", fmt.Sprintf("addresses/%s", id), nil, &address)
+	_, err := a.client.Request("GET", fmt.Sprintf("addresses/%d", id), nil, &address)
 	return &address, err
+}
+
+// GetByStringID returns a specific address by string ID (convenience method)
+func (a *AddressesService) GetByStringID(id string) (*Address, error) {
+	idInt, err := strconv.Atoi(id)
+	if err != nil {
+		return nil, fmt.Errorf("invalid address ID: %w", err)
+	}
+	return a.Get(idInt)
 }
 
 // GetAll returns all addresses in all sections
@@ -63,17 +72,26 @@ func (a *AddressesService) GetAll() ([]Address, error) {
 }
 
 // Ping checks the status of an address
-func (a *AddressesService) Ping(id string) (map[string]interface{}, error) {
+func (a *AddressesService) Ping(id int) (map[string]interface{}, error) {
 	var result map[string]interface{}
-	_, err := a.client.Request("GET", fmt.Sprintf("addresses/%s/ping", id), nil, &result)
+	_, err := a.client.Request("GET", fmt.Sprintf("addresses/%d/ping", id), nil, &result)
 	return result, err
 }
 
 // GetByIPAndSubnet returns an address from a subnet by IP address
-func (a *AddressesService) GetByIPAndSubnet(ip string, subnetID string) (*Address, error) {
+func (a *AddressesService) GetByIPAndSubnet(ip string, subnetID int) (*Address, error) {
 	var address Address
-	_, err := a.client.Request("GET", fmt.Sprintf("addresses/%s/%s", ip, subnetID), nil, &address)
+	_, err := a.client.Request("GET", fmt.Sprintf("addresses/%s/%d", ip, subnetID), nil, &address)
 	return &address, err
+}
+
+// GetByIPAndSubnetString returns an address using a string subnet ID (convenience method)
+func (a *AddressesService) GetByIPAndSubnetString(ip string, subnetID string) (*Address, error) {
+	subnetIDInt, err := strconv.Atoi(subnetID)
+	if err != nil {
+		return nil, fmt.Errorf("invalid subnet ID: %w", err)
+	}
+	return a.GetByIPAndSubnet(ip, subnetIDInt)
 }
 
 // Search searches for addresses in database by IP
@@ -112,9 +130,9 @@ func (a *AddressesService) SearchByMAC(mac string) ([]Address, error) {
 }
 
 // GetFirstFree returns the first available address in a subnet
-func (a *AddressesService) GetFirstFree(subnetID string) (string, error) {
+func (a *AddressesService) GetFirstFree(subnetID int) (string, error) {
 	var firstFree string
-	_, err := a.client.Request("GET", fmt.Sprintf("addresses/first_free/%s", subnetID), nil, &firstFree)
+	_, err := a.client.Request("GET", fmt.Sprintf("addresses/first_free/%d", subnetID), nil, &firstFree)
 	return firstFree, err
 }
 
@@ -133,16 +151,16 @@ func (a *AddressesService) GetTags() ([]Tag, error) {
 }
 
 // GetTag returns a specific address tag
-func (a *AddressesService) GetTag(id string) (*Tag, error) {
+func (a *AddressesService) GetTag(id int) (*Tag, error) {
 	var tag Tag
-	_, err := a.client.Request("GET", fmt.Sprintf("addresses/tags/%s", id), nil, &tag)
+	_, err := a.client.Request("GET", fmt.Sprintf("addresses/tags/%d", id), nil, &tag)
 	return &tag, err
 }
 
 // GetAddressesByTag returns addresses for a specific tag
-func (a *AddressesService) GetAddressesByTag(id string) ([]Address, error) {
+func (a *AddressesService) GetAddressesByTag(id int) ([]Address, error) {
 	var addresses []Address
-	_, err := a.client.Request("GET", fmt.Sprintf("addresses/tags/%s/addresses", id), nil, &addresses)
+	_, err := a.client.Request("GET", fmt.Sprintf("addresses/tags/%d/addresses", id), nil, &addresses)
 	return addresses, err
 }
 
@@ -155,24 +173,24 @@ func (a *AddressesService) Create(address *Address) (*Address, error) {
 	}
 
 	// If we got an ID in the response but not in the address data, retrieve the full address
-	if resp.ID != 0 && createdAddress.ID == "" {
-		return a.Get(strconv.Itoa(resp.ID))
+	if resp.ID != 0 && createdAddress.ID == 0 {
+		return a.Get(resp.ID)
 	}
 
 	return &createdAddress, nil
 }
 
 // CreateFirstFree creates a new address in a subnet - first available
-func (a *AddressesService) CreateFirstFree(subnetID string, address *Address) (*Address, error) {
+func (a *AddressesService) CreateFirstFree(subnetID int, address *Address) (*Address, error) {
 	var createdAddress Address
-	resp, err := a.client.Request("POST", fmt.Sprintf("addresses/first_free/%s", subnetID), address, &createdAddress)
+	resp, err := a.client.Request("POST", fmt.Sprintf("addresses/first_free/%d", subnetID), address, &createdAddress)
 	if err != nil {
 		return nil, err
 	}
 
 	// If we got an ID in the response but not in the address data, retrieve the full address
-	if resp.ID != 0 && createdAddress.ID == "" {
-		return a.Get(strconv.Itoa(resp.ID))
+	if resp.ID != 0 && createdAddress.ID == 0 {
+		return a.Get(resp.ID)
 	}
 
 	return &createdAddress, nil
@@ -180,30 +198,30 @@ func (a *AddressesService) CreateFirstFree(subnetID string, address *Address) (*
 
 // Update updates an address
 func (a *AddressesService) Update(address *Address) (*Address, error) {
-	if address.ID == "" {
+	if address.ID == 0 {
 		return nil, fmt.Errorf("address ID is required for update")
 	}
 
 	var updatedAddress Address
-	_, err := a.client.Request("PATCH", fmt.Sprintf("addresses/%s", address.ID), address, &updatedAddress)
+	_, err := a.client.Request("PATCH", fmt.Sprintf("addresses/%d", address.ID), address, &updatedAddress)
 	return &updatedAddress, err
 }
 
 // Delete deletes an address
-func (a *AddressesService) Delete(id string) error {
-	_, err := a.client.Request("DELETE", fmt.Sprintf("addresses/%s", id), nil, nil)
+func (a *AddressesService) Delete(id int) error {
+	_, err := a.client.Request("DELETE", fmt.Sprintf("addresses/%d", id), nil, nil)
 	return err
 }
 
 // DeleteWithRemoveDNS deletes an address and removes all related DNS records
-func (a *AddressesService) DeleteWithRemoveDNS(id string) error {
+func (a *AddressesService) DeleteWithRemoveDNS(id int) error {
 	params := map[string]string{"remove_dns": "1"}
-	_, err := a.client.Request("DELETE", fmt.Sprintf("addresses/%s", id), params, nil)
+	_, err := a.client.Request("DELETE", fmt.Sprintf("addresses/%d", id), params, nil)
 	return err
 }
 
 // DeleteByIPAndSubnet deletes an address by IP in a specific subnet
-func (a *AddressesService) DeleteByIPAndSubnet(ip string, subnetID string) error {
-	_, err := a.client.Request("DELETE", fmt.Sprintf("addresses/%s/%s", ip, subnetID), nil, nil)
+func (a *AddressesService) DeleteByIPAndSubnet(ip string, subnetID int) error {
+	_, err := a.client.Request("DELETE", fmt.Sprintf("addresses/%s/%d", ip, subnetID), nil, nil)
 	return err
 }
--- pkg/phpipam/subnets.go
+++ pkg/phpipam/subnets.go
@@ -10,26 +10,26 @@ type Subnet struct {
 	ID             int    `json:"id,omitempty"`
 	Subnet         string `json:"subnet"`
 	Mask           string `json:"mask"`
-	SectionID      string `json:"sectionId,omitempty"`
+	SectionID      int    `json:"sectionId,omitempty"`
 	Description    string `json:"description,omitempty"`
-	LinkedSubnet   string `json:"linked_subnet,omitempty"`
-	VlanID         string `json:"vlanId,omitempty"`
-	VrfID          string `json:"vrfId,omitempty"`
-	MasterSubnetID string `json:"masterSubnetId,omitempty"`
-	NameserverID   string `json:"nameserverId,omitempty"`
-	ShowName       string `json:"showName,omitempty"`
+	LinkedSubnet   int    `json:"linked_subnet,omitempty"`
+	VlanID         int    `json:"vlanId,omitempty"`
+	VrfID          int    `json:"vrfId,omitempty"`
+	MasterSubnetID int    `json:"masterSubnetId,omitempty"`
+	NameserverID   int    `json:"nameserverId,omitempty"`
+	ShowName       int    `json:"showName,omitempty"`
 	Permissions    string `json:"permissions,omitempty"`
-	DNSRecursive   string `json:"DNSrecursive,omitempty"`
-	DNSRecords     string `json:"DNSrecords,omitempty"`
-	AllowRequests  string `json:"allowRequests,omitempty"`
-	ScanAgent      string `json:"scanAgent,omitempty"`
-	PingSubnet     string `json:"pingSubnet,omitempty"`
-	DiscoverSubnet string `json:"discoverSubnet,omitempty"`
-	IsFolder       string `json:"isFolder,omitempty"`
-	IsFull         string `json:"isFull,omitempty"`
-	State          string `json:"state,omitempty"`
-	Threshold      string `json:"threshold,omitempty"`
-	Location       string `json:"location,omitempty"`
+	DNSRecursive   int    `json:"DNSrecursive,omitempty"`
+	DNSRecords     int    `json:"DNSrecords,omitempty"`
+	AllowRequests  int    `json:"allowRequests,omitempty"`
+	ScanAgent      int    `json:"scanAgent,omitempty"`
+	PingSubnet     int    `json:"pingSubnet,omitempty"`
+	DiscoverSubnet int    `json:"discoverSubnet,omitempty"`
+	IsFolder       int    `json:"isFolder,omitempty"`
+	IsFull         int    `json:"isFull,omitempty"`
+	State          int    `json:"state,omitempty"`
+	Threshold      int    `json:"threshold,omitempty"`
+	Location       int    `json:"location,omitempty"`
 	EditDate       string `json:"editDate,omitempty"`
 }
 
@@ -59,72 +59,81 @@ func (s *SubnetsService) List() ([]Subnet, error) {
 }
 
 // Get returns a specific subnet by ID
-func (s *SubnetsService) Get(id string) (*Subnet, error) {
+func (s *SubnetsService) Get(id int) (*Subnet, error) {
 	var subnet Subnet
-	_, err := s.client.Request("GET", fmt.Sprintf("subnets/%s", id), nil, &subnet)
+	_, err := s.client.Request("GET", fmt.Sprintf("subnets/%d", id), nil, &subnet)
 	return &subnet, err
 }
 
+// GetByStringID returns a specific subnet by string ID (convenience method)
+func (s *SubnetsService) GetByStringID(id string) (*Subnet, error) {
+	idInt, err := strconv.Atoi(id)
+	if err != nil {
+		return nil, fmt.Errorf("invalid subnet ID: %w", err)
+	}
+	return s.Get(idInt)
+}
+
 // GetUsage returns usage statistics for a subnet
-func (s *SubnetsService) GetUsage(id string) (*SubnetUsage, error) {
+func (s *SubnetsService) GetUsage(id int) (*SubnetUsage, error) {
 	var usage SubnetUsage
-	_, err := s.client.Request("GET", fmt.Sprintf("subnets/%s/usage", id), nil, &usage)
+	_, err := s.client.Request("GET", fmt.Sprintf("subnets/%d/usage", id), nil, &usage)
 	return &usage, err
 }
 
 // GetSlaves returns all immediate slave subnets
-func (s *SubnetsService) GetSlaves(id string) ([]Subnet, error) {
+func (s *SubnetsService) GetSlaves(id int) ([]Subnet, error) {
 	var subnets []Subnet
-	_, err := s.client.Request("GET", fmt.Sprintf("subnets/%s/slaves", id), nil, &subnets)
+	_, err := s.client.Request("GET", fmt.Sprintf("subnets/%d/slaves", id), nil, &subnets)
 	return subnets, err
 }
 
 // GetSlavesRecursive returns all slave subnets recursively
-func (s *SubnetsService) GetSlavesRecursive(id string) ([]Subnet, error) {
+func (s *SubnetsService) GetSlavesRecursive(id int) ([]Subnet, error) {
 	var subnets []Subnet
-	_, err := s.client.Request("GET", fmt.Sprintf("subnets/%s/slaves_recursive", id), nil, &subnets)
+	_, err := s.client.Request("GET", fmt.Sprintf("subnets/%d/slaves_recursive", id), nil, &subnets)
 	return subnets, err
 }
 
 // GetAddresses returns all addresses in a subnet
-func (s *SubnetsService) GetAddresses(id string) ([]Address, error) {
+func (s *SubnetsService) GetAddresses(id int) ([]Address, error) {
 	var addresses []Address
-	_, err := s.client.Request("GET", fmt.Sprintf("subnets/%s/addresses", id), nil, &addresses)
+	_, err := s.client.Request("GET", fmt.Sprintf("subnets/%d/addresses", id), nil, &addresses)
 	return addresses, err
 }
 
 // GetAddress returns a specific IP address from a subnet
-func (s *SubnetsService) GetAddress(id, ip string) (*Address, error) {
+func (s *SubnetsService) GetAddress(id int, ip string) (*Address, error) {
 	var address Address
-	_, err := s.client.Request("GET", fmt.Sprintf("subnets/%s/addresses/%s", id, ip), nil, &address)
+	_, err := s.client.Request("GET", fmt.Sprintf("subnets/%d/addresses/%s", id, ip), nil, &address)
 	return &address, err
 }
 
 // GetFirstFree returns the first available IP address in a subnet
-func (s *SubnetsService) GetFirstFree(id string) (string, error) {
+func (s *SubnetsService) GetFirstFree(id int) (string, error) {
 	var firstFree string
-	_, err := s.client.Request("GET", fmt.Sprintf("subnets/%s/first_free", id), nil, &firstFree)
+	_, err := s.client.Request("GET", fmt.Sprintf("subnets/%d/first_free", id), nil, &firstFree)
 	return firstFree, err
 }
 
 // GetFirstSubnet returns the first available subnet within a given subnet for specified mask
-func (s *SubnetsService) GetFirstSubnet(id string, mask int) (string, error) {
+func (s *SubnetsService) GetFirstSubnet(id int, mask int) (string, error) {
 	var firstSubnet string
-	_, err := s.client.Request("GET", fmt.Sprintf("subnets/%s/first_subnet/%d", id, mask), nil, &firstSubnet)
+	_, err := s.client.Request("GET", fmt.Sprintf("subnets/%d/first_subnet/%d", id, mask), nil, &firstSubnet)
 	return firstSubnet, err
 }
 
 // GetLastSubnet returns the last available subnet within a given subnet for specified mask
-func (s *SubnetsService) GetLastSubnet(id string, mask int) (string, error) {
+func (s *SubnetsService) GetLastSubnet(id int, mask int) (string, error) {
 	var lastSubnet string
-	_, err := s.client.Request("GET", fmt.Sprintf("subnets/%s/last_subnet/%d", id, mask), nil, &lastSubnet)
+	_, err := s.client.Request("GET", fmt.Sprintf("subnets/%d/last_subnet/%d", id, mask), nil, &lastSubnet)
 	return lastSubnet, err
 }
 
 // GetAllSubnets returns all available subnets within a given subnet for specified mask
-func (s *SubnetsService) GetAllSubnets(id string, mask int) ([]string, error) {
+func (s *SubnetsService) GetAllSubnets(id int, mask int) ([]string, error) {
 	var allSubnets []string
-	_, err := s.client.Request("GET", fmt.Sprintf("subnets/%s/all_subnets/%d", id, mask), nil, &allSubnets)
+	_, err := s.client.Request("GET", fmt.Sprintf("subnets/%d/all_subnets/%d", id, mask), nil, &allSubnets)
 	return allSubnets, err
 }
 
@@ -158,24 +167,24 @@ func (s *SubnetsService) Create(subnet *Subnet) (*Subnet, error) {
 	}
 
 	// If we got an ID in the response but not in the subnet data, retrieve the full subnet
-	if resp.ID != 0 && createdSubnet.ID == "" {
-		return s.Get(strconv.Itoa(resp.ID))
+	if resp.ID != 0 && createdSubnet.ID == 0 {
+		return s.Get(resp.ID)
 	}
 
 	return &createdSubnet, nil
 }
 
 // CreateFirstSubnet creates a new child subnet inside a subnet with specified mask
-func (s *SubnetsService) CreateFirstSubnet(id string, mask int, subnet *Subnet) (*Subnet, error) {
+func (s *SubnetsService) CreateFirstSubnet(id int, mask int, subnet *Subnet) (*Subnet, error) {
 	var createdSubnet Subnet
-	resp, err := s.client.Request("POST", fmt.Sprintf("subnets/%s/first_subnet/%d", id, mask), subnet, &createdSubnet)
+	resp, err := s.client.Request("POST", fmt.Sprintf("subnets/%d/first_subnet/%d", id, mask), subnet, &createdSubnet)
 	if err != nil {
 		return nil, err
 	}
 
 	// If we got an ID in the response but not in the subnet data, retrieve the full subnet
-	if resp.ID != 0 && createdSubnet.ID == "" {
-		return s.Get(strconv.Itoa(resp.ID))
+	if resp.ID != 0 && createdSubnet.ID == 0 {
+		return s.Get(resp.ID)
 	}
 
 	return &createdSubnet, nil
@@ -183,49 +192,49 @@ func (s *SubnetsService) CreateFirstSubnet(id string, mask int, subnet *Subnet) 
 
 // Update updates an existing subnet
 func (s *SubnetsService) Update(subnet *Subnet) (*Subnet, error) {
-	if subnet.ID == "" {
+	if subnet.ID == 0 {
 		return nil, fmt.Errorf("subnet ID is required for update")
 	}
 
 	var updatedSubnet Subnet
-	_, err := s.client.Request("PATCH", fmt.Sprintf("subnets/%s", subnet.ID), subnet, &updatedSubnet)
+	_, err := s.client.Request("PATCH", fmt.Sprintf("subnets/%d", subnet.ID), subnet, &updatedSubnet)
 	return &updatedSubnet, err
 }
 
 // Resize resizes a subnet to a new mask
-func (s *SubnetsService) Resize(id string, mask int) error {
+func (s *SubnetsService) Resize(id int, mask int) error {
 	data := map[string]int{"mask": mask}
-	_, err := s.client.Request("PATCH", fmt.Sprintf("subnets/%s/resize", id), data, nil)
+	_, err := s.client.Request("PATCH", fmt.Sprintf("subnets/%d/resize", id), data, nil)
 	return err
 }
 
 // Split splits a subnet into smaller subnets
-func (s *SubnetsService) Split(id string, subnets int) error {
+func (s *SubnetsService) Split(id int, subnets int) error {
 	data := map[string]int{"number": subnets}
-	_, err := s.client.Request("PATCH", fmt.Sprintf("subnets/%s/split", id), data, nil)
+	_, err := s.client.Request("PATCH", fmt.Sprintf("subnets/%d/split", id), data, nil)
 	return err
 }
 
 // SetPermissions sets subnet permissions
-func (s *SubnetsService) SetPermissions(id string, permissions map[string]string) error {
-	_, err := s.client.Request("PATCH", fmt.Sprintf("subnets/%s/permissions", id), permissions, nil)
+func (s *SubnetsService) SetPermissions(id int, permissions map[string]string) error {
+	_, err := s.client.Request("PATCH", fmt.Sprintf("subnets/%d/permissions", id), permissions, nil)
 	return err
 }
 
 // Delete deletes a subnet
-func (s *SubnetsService) Delete(id string) error {
-	_, err := s.client.Request("DELETE", fmt.Sprintf("subnets/%s", id), nil, nil)
+func (s *SubnetsService) Delete(id int) error {
+	_, err := s.client.Request("DELETE", fmt.Sprintf("subnets/%d", id), nil, nil)
 	return err
 }
 
 // Truncate removes all addresses from a subnet
-func (s *SubnetsService) Truncate(id string) error {
-	_, err := s.client.Request("DELETE", fmt.Sprintf("subnets/%s/truncate", id), nil, nil)
+func (s *SubnetsService) Truncate(id int) error {
+	_, err := s.client.Request("DELETE", fmt.Sprintf("subnets/%d/truncate", id), nil, nil)
 	return err
 }
 
 // RemovePermissions removes all permissions from a subnet
-func (s *SubnetsService) RemovePermissions(id string) error {
-	_, err := s.client.Request("DELETE", fmt.Sprintf("subnets/%s/permissions", id), nil, nil)
+func (s *SubnetsService) RemovePermissions(id int) error {
+	_, err := s.client.Request("DELETE", fmt.Sprintf("subnets/%d/permissions", id), nil, nil)
 	return err
 }
